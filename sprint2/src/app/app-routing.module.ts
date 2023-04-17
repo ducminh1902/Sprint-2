7a3d7d844e56b0172702661eec07281abcf41134
unknown <leducminh22003@gmail.com>
2023-04-17 11:12:52 +0700

    nop

--- sprint2/src/app/app-routing.module.ts
+++ sprint2/src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {HomePageComponent} from "./home-page/home-page.component";
+import {LoginComponent} from "./login/login.component";
 
 
-const routes: Routes = [{
-  path:"",component:HomePageComponent,
-}];
+const routes: Routes = [
+  { path: "", component: HomePageComponent},
+  {path: "login", component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
